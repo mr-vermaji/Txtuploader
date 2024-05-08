d1ac67272486dee9780e3482fe0eacc8f6666d7e
mr-vermaji <167828828+mr-vermaji@users.noreply.github.com>
2024-05-08 06:53:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 28947698
 api_hash = "181af3baf73a37c24a429ca08e3f34f8"
-bot_token = "7155052855:AAFOUdjanxH-ZPsvmChdRWKvH3cg24Qf4Vg"
+bot_token = "7014051423:AAGw4VnXwrPA0Bd6BsoYUlgh708-ZTDmn6k"
 auth_users = "7035612796, 1406300051, 6801378994, 870101201"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7035612796"
